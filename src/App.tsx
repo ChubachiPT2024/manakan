4a3ee7321b03881229e4a047b719fe63c789cbde
shunya9811 <11wash27@gmail.com>
2024-06-22 00:00:20 +0900

    デザインなしのタイトルとヘッダーを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
+import CommonLayout from "./common/CommonLayout";
+
+
 export default function App() {
     return (
-        <>
-            <h1>Hello from React!</h1>
-        </>
-    )
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<CommonLayout />}>
+                    <Route index element={<h1>Home Page</h1>} />
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
 }
