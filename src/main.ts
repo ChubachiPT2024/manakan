a44133f362552b5f8db5b2197c89016757fad86a
shunya9811 <11wash27@gmail.com>
2024-06-21 14:30:15 +0900

    ディレクトリ構造の変更

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
