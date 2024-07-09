813f3304d6d6d47799f70f52b00bea16925dd8aa
a2307yo <a2307yo@aiit.ac.jp>
2024-07-09 16:21:41 +0900

    ・不要なコメントの削除 ・タイトルをManakanに変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 // ページコンポーネントのインポート
 import Home from './presentation/pages/Home'
 import Classification from './presentation/pages/Classification'
-// import Evaluation from './presentation/pages/Evaluation'
-// import Test from './presentation/pages/Pages'
 
 export default function App() {
   return (
