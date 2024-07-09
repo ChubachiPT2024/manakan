6ab03353326fbad3a6b6ef51cebe80b84fc963bc
a2307yo <a2307yo@aiit.ac.jp>
2024-07-09 14:03:52 +0900

    列の幅を修正

--- src/presentation/classification/components/GradeColumn.tsx
+++ src/presentation/classification/components/GradeColumn.tsx
@@ -12,12 +12,12 @@ export function GradeColumn({
   submissionNum,
 }: GradeColumnProps) {
   return (
-    <div className="min-w-100 mr-2   bg-white rounded-md p-2">
+    <div className="min-w-[270px] mr-2 bg-white rounded-md p-2">
       <div className="flex justify-between border-b border-b-gray-200 mb-2">
         <h2 className="text-3xl">{title}</h2>
         <p className="text-xs self-end mb-1">合計:{submissionNum}</p>
       </div>
-      <div className="h-auto overflow-y-scroll">{children}</div>
+      <div className="min-w-[260px] h-auto overflow-y-scroll">{children}</div>
     </div>
   )
 }
