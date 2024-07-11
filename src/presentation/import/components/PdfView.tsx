7aa3b344107d2a7d31fc2e1accce3a2071b11f24
rt4716 <ryuichi.takashima@gmail.com>
2024-07-11 22:54:02 +0900

    split import/components/PdfView.tsx from Review.tsx

---
+++ src/presentation/import/components/PdfView.tsx
@@ -0,0 +1,63 @@
+// presentation/import/components/PdfView.tsx
+
+import React, { useEffect, useState } from 'react'
+import { Document, Page } from 'react-pdf'
+
+interface PdfViewProps {
+  studentName: string
+  pdfPaths: string[]
+  height: string
+  width: number
+  pageHeight: number
+}
+
+const PdfView: React.FC<PdfViewProps> = ({
+  studentName,
+  pdfPaths,
+  height,
+  width,
+  pageHeight,
+}) => {
+  const [pdfDatas, setPdfDatas] = useState<Uint8Array[]>([])
+
+  useEffect(() => {
+    const fetchPdfFiles = async () => {
+      const pdfDataPromises = pdfPaths.map(async (path) => {
+        const response = await fetch(path)
+        const arrayBuffer = await response.arrayBuffer()
+        return new Uint8Array(arrayBuffer)
+      })
+
+      const pdfDataArray = await Promise.all(pdfDataPromises)
+      setPdfDatas(pdfDataArray)
+    }
+
+    fetchPdfFiles()
+  }, [pdfPaths])
+
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl font-bold">{studentName}</h2>
+      <div className="inline-block overflow-y-auto m-2" style={{ height }}>
+        {pdfDatas.map((data, index) => (
+          <div
+            key={index}
+            className="mb-5 bg-red-500"
+            style={{ width, height: pageHeight }}
+          >
+            <Document file={{ data }}>
+              {Array.from(new Array(1), (_, pageIndex) => (
+                <Page
+                  key={`${studentName}-page-${pageIndex + 1}`}
+                  pageNumber={pageIndex + 1}
+                />
+              ))}
+            </Document>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default PdfView
