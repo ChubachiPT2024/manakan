c7092cec80adfa2be2ddb7fb075fac97402c3741
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-07-14 16:32:57 +0900

    assessment.getScore() を実装

--- src/domain/models/assessments/assessment.ts
+++ src/domain/models/assessments/assessment.ts
@@ -111,4 +111,41 @@ export class Assessment {
       score
     )
   }
+
+  /**
+   * 点数を取得する
+   *
+   * @returns 点数
+   */
+  public getScore(): number {
+    if (this.grade === undefined) {
+      throw new Error('The grade must be defined.')
+    }
+
+    if (this.grade === 0) {
+      return 0
+    }
+
+    const rankIndex = ['--', '-', '+-', '+', '++'].indexOf(this.rank)
+    if (rankIndex === -1) {
+      throw new Error(
+        'The rank must be defined if the grade is defined and not 0.'
+      )
+    }
+
+    switch (this.grade) {
+      case 1:
+        return 10 + 10 * rankIndex
+      case 2:
+        return 60 + 2 * rankIndex
+      case 3:
+        return 70 + 2 * rankIndex
+      case 4:
+        return 80 + 2 * rankIndex
+      case 5:
+        // '++' のとき 100 点になるようにする
+        // 小数は入力できない可能性があるので四捨五入する（要確認）
+        return Math.round(90 + 2.5 * rankIndex)
+    }
+  }
 }
