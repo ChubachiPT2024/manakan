46a7f0a430d454ad5e5ddf138c32b5924b5325cc
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-07-15 17:39:20 +0900

    インポート時、提出済かどうかを保存するようにする

---
+++ src/application/submissionSummaries/submissionSummarySubmissionData.ts
@@ -0,0 +1,32 @@
+import { Submission } from 'src/domain/models/submissions/submission'
+
+/**
+ * 提出物サマリ提出物データ
+ */
+export class SubmissionSummarySubmissionData {
+  /**
+   * 提出済かどうか
+   */
+  public readonly isSubmitted: boolean
+
+  /**
+   *　提出日時
+   */
+  public readonly submissionDateTime?: string
+
+  /**
+   * 提出回数
+   */
+  public readonly submissionCount?: number
+
+  /**
+   * コンストラクタ
+   *
+   * @param student 学生
+   */
+  public constructor(submission: Submission) {
+    this.isSubmitted = submission.isSubmitted
+    this.submissionDateTime = submission.submissionDateTime
+    this.submissionCount = submission.submissionCount
+  }
+}
