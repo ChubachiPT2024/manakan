697a031612bd9d9ec2b31945a9fbd5cbc9ebdfc4
a2307yo <a2307yo@aiit.ac.jp>
2024-07-16 23:40:27 +0900

    src\presentation\pages\Review.tsxのみmainから手動でマージ

--- src/presentation/pages/Review.tsx
+++ src/presentation/pages/Review.tsx
@@ -36,155 +36,20 @@ const Review = () => {
         </div>
 
         {/* メインコンテンツ */}
-        <div className='flex mt-14' style={{ height: 'calc(100vh - 4rem)' }}>
-
+        <div className="flex mt-14" style={{ height: 'calc(100vh - 4rem)' }}>
           {/* PDF表示 */}
           <div className="w-10/12 pl-8 pt-5 overflow-x-auto whitespace-nowrap flex overflow-y-hidden">
-            <div className='text-center'>
-              <h2 className='text-2xl font-bold'>学生1</h2>
-              <div className="inline-block overflow-y-auto m-2" style={{ height: 'calc(100vh - 4rem)' }}>
-                <div
-                  className='mb-5 bg-red-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 1-1
-                </div>
-                <div
-                  className='mb-5 bg-red-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 1-2
-                </div>
-                <div
-                  className='bg-red-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 1-3
-                </div>
-              </div>
-            </div>
-            <div className='text-center'>
-              <h2 className='text-2xl font-bold'>学生2</h2>
-              <div className="inline-block overflow-y-auto m-2" style={{ height: 'calc(100vh - 3.5rem)' }}>
-                <div
-                  className='mb-5 bg-blue-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 2-1
-                </div>
-                <div
-                  className='mb-5 bg-blue-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 2-2
-                </div>
-                <div
-                  className='bg-blue-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 2-3
-                </div>
-              </div>
-            </div>
-            <div className='text-center'>
-              <h2 className='text-2xl font-bold'>学生3</h2>
-              <div className="inline-block overflow-y-auto m-2" style={{ height: 'calc(100vh - 3.5rem)' }}>
-                <div
-                  className='mb-5 bg-green-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 3-1
-                </div>
-                <div
-                  className='mb-5 bg-green-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 3-2
-                </div>
-                <div
-                  className='bg-green-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 3-3
-                </div>
-              </div>
-            </div>
-            <div className='text-center'>
-              <h2 className='text-2xl font-bold'>学生4</h2>
-              <div className="inline-block overflow-y-auto m-2" style={{ height: 'calc(100vh - 3.5rem)' }}>
-                <div
-                  className='mb-5 bg-pink-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 4-1
-                </div>
-                <div
-                  className='mb-5 bg-pink-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 4-2
-                </div>
-                <div
-                  className='bg-pink-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 4-3
-                </div>
-              </div>
-            </div>
-            <div className='text-center'>
-              <h2 className='text-2xl font-bold'>学生5</h2>
-              <div className="inline-block overflow-y-auto m-2" style={{ height: 'calc(100vh - 3.5rem)' }}>
-                <div
-                  className='mb-5 bg-purple-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 5-1
-                </div>
-                <div
-                  className='mb-5 bg-purple-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 5-2
-                </div>
-                <div
-                  className='bg-purple-500'
-                  style={{
-                  width: 900,
-                  height: 1000,
-                }}>
-                  PDF 5-3
-                </div>
-              </div>
-            </div>
+            {submissionSummaries.map((summary, index) => (
+              <PdfView
+                key={index}
+                reportId={Number(reportId)}
+                student={summary.student}
+                files={summary.files}
+                height="calc(100vh - 4rem)"
+                width={900}
+                pageHeight={1000}
+              />
+            ))}
           </div>
 
           <Sidebar
@@ -195,6 +60,6 @@ const Review = () => {
       </div>
     </>
   )
-};
+}
 
-export default Review;
+export default Review
