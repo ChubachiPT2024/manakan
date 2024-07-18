b242fcc5527cd076aff82ad7d3803025e1b9ff08
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-07-18 20:01:53 +0900

    JsonSubmissionRepository を実装

---
+++ src/infrastructure/json/submissions/jsonSubmissionRepository.ts
@@ -0,0 +1,92 @@
+import { Submission } from 'src/domain/models/submissions/submission'
+import { SubmissionRepository } from 'src/domain/models/submissions/submissionRepository'
+import { readFile, writeFile } from 'node:fs/promises'
+
+/**
+ * JSON 提出物リポジトリ
+ */
+export class JsonSubmissionRepository implements SubmissionRepository {
+  /**
+   * コンストラクタ
+   *
+   * @param jsonFileAbsolutePath JSON ファイルの絶対パス
+   */
+  public constructor(private readonly jsonFileAbsolutePath: string) {}
+
+  /**
+   * 提出物を保存する
+   *
+   * @param submission 提出物
+   */
+  public async saveAsync(submission: Submission): Promise<void> {
+    const submissions = await this.readFromJsonFileAsync()
+
+    const index = submissions.findIndex(
+      (x) =>
+        x.reportId === submission.reportId &&
+        x.studentNumId === submission.studentNumId
+    )
+    if (index === -1) {
+      submissions.push(submission)
+    } else {
+      submissions[index] = submission
+    }
+
+    await this.writeToJsonFileAsync(submissions)
+  }
+
+  /**
+   * 提出物をレポート ID で検索する
+   *
+   * @param reportId レポート ID
+   * @returns 提出物
+   */
+  public async findByReportIdAsync(reportId: number): Promise<Submission[]> {
+    const submissions = await this.readFromJsonFileAsync()
+    return submissions.filter((x) => x.reportId === reportId)
+  }
+
+  /**
+   * 提出物を検索する
+   *
+   * @param reportId レポート ID
+   * @param studentNumId 学籍番号
+   * @returns 提出物
+   */
+  public async findAsync(
+    reportId: number,
+    studentNumId: number
+  ): Promise<Submission> {
+    const submissions = await this.readFromJsonFileAsync()
+    const submission = submissions.find(
+      (x) => x.reportId === reportId && x.studentNumId === studentNumId
+    )
+    if (!submission) {
+      throw new Error('The submission is not found.')
+    }
+    return submission
+  }
+
+  /**
+   * 提出物を JSON ファイルから読み込む
+   *
+   * @returns 提出物
+   */
+  private async readFromJsonFileAsync(): Promise<Submission[]> {
+    try {
+      return JSON.parse(await readFile(this.jsonFileAbsolutePath, 'utf8'))
+    } catch {
+      // まだファイルが存在しない場合
+      return []
+    }
+  }
+
+  /**
+   * 提出物を JSON ファイルに書き込む
+   *
+   * @param submissions 提出物
+   */
+  private async writeToJsonFileAsync(submissions: Submission[]): Promise<void> {
+    await writeFile(this.jsonFileAbsolutePath, JSON.stringify(submissions))
+  }
+}
