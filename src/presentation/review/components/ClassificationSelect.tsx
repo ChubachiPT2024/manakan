b9d4ae5a82a67ee31b2cd4dea255ef1e18fdacab
shunya9811 <11wash27@gmail.com>
2024-07-19 20:02:32 +0900

    scoreは取得したものをそのまま表示するように変更

--- src/presentation/review/components/ClassificationSelect.tsx
+++ src/presentation/review/components/ClassificationSelect.tsx
@@ -45,21 +45,10 @@ const ClassificationSelect: React.FC<ClassificationSelectProps> = ({
         summary.student.numId === selectedStudent.numId
     )?.assessment.rank ?? ''
 
-  // 選択された学生の点数
-  const initialSelectedScore =
-    submissionSummaries.find(
-      (summary: SubmissionSummaryData) =>
-        summary.student.numId === selectedStudent.numId
-    )?.assessment.score ?? 0
-
   // 評点の値を状態として管理
   const [selectedGradeRank, setSelectedGradeRank] = useState<string>(
     initialSelectedGrade + initialSelectedRank
   )
-
-  // 点数の値を状態として管理
-  const [selectedScode, setSelectedScore] =
-    useState<number>(initialSelectedScore)
 
   // 評点が変更されたら、評点を更新する
   const handleChange = async (event: SelectChangeEvent<string>) => {
@@ -76,16 +65,12 @@ const ClassificationSelect: React.FC<ClassificationSelectProps> = ({
       )
       // SWRのキャッシュを更新
       mutate('SubmissionSummaries')
-
-      // Todo: 点数の更新
-      // 評点が変わったら、即座に点数も更新したいが、
-      // メインプロセスからもってくる方針だとレンダリングされない
     } catch (e: Error | any) {
       console.log(e)
     }
   }
 
-  // selectedStudent が変わるたびにGradeとRankとScoreの値を更新
+  // selectedStudent が変わるたびにGradeとRankの値を更新
   // それぞれ、新しい学生の値を取得して、状態にセット
   useEffect(() => {
     const newGrade =
@@ -98,14 +83,7 @@ const ClassificationSelect: React.FC<ClassificationSelectProps> = ({
         (summary: SubmissionSummaryData) =>
           summary.student.numId === selectedStudent.numId
       )?.assessment.rank ?? ''
-    const newScore =
-      submissionSummaries.find(
-        (summary: SubmissionSummaryData) =>
-          summary.student.numId === selectedStudent.numId
-      )?.assessment.score ?? 0
-
     setSelectedGradeRank(newGrade + newRank)
-    setSelectedScore(newScore)
   }, [selectedStudent])
 
   return (
@@ -143,7 +121,12 @@ const ClassificationSelect: React.FC<ClassificationSelectProps> = ({
             id="score"
             type="number"
             disabled
-            value={selectedScode}
+            value={
+              submissionSummaries.find(
+                (summary: SubmissionSummaryData) =>
+                  summary.student.numId === selectedStudent.numId
+              )?.assessment.score ?? 0
+            }
             variant="outlined"
             fullWidth
             inputProps={{
