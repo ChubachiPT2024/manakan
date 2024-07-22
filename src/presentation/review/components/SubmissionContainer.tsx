cb10bd6aaa98f165711d5e32d0c2ae03aa14283c
rt4716 <ryuichi.takashima@gmail.com>
2024-07-22 13:20:37 +0900

    add some comments

--- src/presentation/review/components/SubmissionContainer.tsx
+++ src/presentation/review/components/SubmissionContainer.tsx
@@ -9,6 +9,7 @@ const SubmissionContainer: React.FC<SubmissionContainerProps> = ({
   height,
   children,
 }) => (
+  // 複数ファイルの一番下が見切れる場合は pb を深くする / なんか自動調整できるようにしたい
   <div className="overflow-y-auto pb-8" style={{ height }}>
     {children}
   </div>
